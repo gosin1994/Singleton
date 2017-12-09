2a354c3c03aaa7b6758bd70bb5da4833c1131748
gosin <979248310@qq>
2017-12-09 11:48:44 +0800

    写了饿汉式
    写了一个饿汉式

--- src/com/laosiji/test/Main.java
+++ src/com/laosiji/test/Main.java
@@ -3,7 +3,18 @@ package com.laosiji.test;
 public class Main {
 
     public static void main(String[] args) {
+        Singleton.getInstance();
 
-        单例模式
+    }
+}
+
+class Singleton {
+    //1.私有构造方法，其他类不能访问该构造方法
+    private Singleton() {};
+    //2.创建本类对象
+    private static Singleton s = new Singleton();
+    //3.对外提供公共的访问方法
+    public static Singleton getInstance() {
+        retrun s;
     }
 }
