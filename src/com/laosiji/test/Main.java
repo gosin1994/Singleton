2e6ae932d9e2e5f23d453b203698a579b6030a0e
gosin <979248310@qq>
2017-12-09 13:01:00 +0800

    添加了饿汉式

--- src/com/laosiji/test/Main.java
+++ src/com/laosiji/test/Main.java
@@ -9,7 +9,10 @@ public class Main {
     }
 }
 
-class Singleton {
+/**
+ * 饿汉式
+ */
+/*class Singleton {
     //1.私有构造方法，其他类不能访问该构造方法
     private Singleton() {};
     //2.创建本类对象
@@ -17,5 +20,22 @@ class Singleton {
     //3.对外提供公共的访问方法
     public static Singleton getInstance() {//获取实例
         retrun s;
+    }
+}*/
+
+/**
+ * 懒汉式(单例的延迟加载模式)
+ */
+class Singleton {
+    //1.私有构造方法，其他类不能访问该构造方法
+    private Singleton() {};
+    //2.声明引用
+    private static Singleton s;
+    //3.对外提供公共的访问方法
+    public static Singleton getInstance() {//获取实例
+        if (s == null) {
+            s == new Singleton();
+        }
+        return s;
     }
 }
